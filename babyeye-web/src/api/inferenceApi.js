4742b267b77896e63c3874d0b2b13cbce6a7c95a
SeokChan <kxkx55402@gmail.com>
2026-05-17 21:43:37 +0900

    fix client error

--- babyeye-web/src/api/inferenceApi.js
+++ babyeye-web/src/api/inferenceApi.js
@@ -1,7 +1,6 @@
-﻿import { apiPostFormData } from './client'
+﻿import { apiGet, apiPostFormData } from './client'
 
 export function requestMultimodalInference({
-  videoFile,
   audioFile,
   frameFiles,
   cameraId,
@@ -10,10 +9,6 @@ export function requestMultimodalInference({
   durationSeconds,
 }) {
   const formData = new FormData()
-
-  if (videoFile) {
-    formData.append('video_file', videoFile)
-  }
 
   if (audioFile) {
     formData.append('audio_file', audioFile)
@@ -40,4 +35,27 @@ export function requestMultimodalInference({
   }
 
   return apiPostFormData('/api/inference/multimodal', formData)
+}
+
+export function getInferenceStatus(requestId) {
+  return apiGet('/api/inference/requests/' + requestId + '/status')
+}
+
+export function getInferenceResult(requestId) {
+  return apiGet('/api/inference/requests/' + requestId + '/result')
+}
+
+export function buildInferenceDebugUrls(apiBaseUrl, requestId) {
+  return {
+    lastFrameUrl:
+      apiBaseUrl + '/api/inference/requests/' + requestId + '/last-frame',
+    audioUrl:
+      apiBaseUrl + '/api/inference/requests/' + requestId + '/audio',
+    metadataUrl:
+      apiBaseUrl + '/api/inference/requests/' + requestId + '/metadata',
+    statusUrl:
+      apiBaseUrl + '/api/inference/requests/' + requestId + '/status',
+    resultUrl:
+      apiBaseUrl + '/api/inference/requests/' + requestId + '/result',
+  }
 }
