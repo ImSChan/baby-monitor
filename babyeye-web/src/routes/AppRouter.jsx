c8166f1e957e89c59bf58bd4b4d7fbf8ea5ece77
ImSChan <kxkx55402@gmail.com>
2026-06-03 19:06:33 +0900

    Fix app router imports

--- babyeye-web/src/routes/AppRouter.jsx
+++ babyeye-web/src/routes/AppRouter.jsx
@@ -1,6 +1,5 @@
 ﻿import { Navigate, Route, Routes } from 'react-router-dom'
 
-import BottomNav from '../components/layout/BottomNav'
 import CameraHostPage from '../pages/CameraHostPage'
 import EmotionPage from '../pages/EmotionPage'
 import HomePage from '../pages/HomePage'
@@ -10,21 +9,15 @@ import SmartHomePage from '../pages/SmartHomePage'
 
 function AppRouter() {
   return (
-    <div className='mx-auto min-h-screen max-w-[430px] bg-slate-950 text-white'>
-      <div className='pb-24'>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/live' element={<LivePage />} />
-          <Route path='/camera-host' element={<CameraHostPage />} />
-          <Route path='/emotion' element={<EmotionPage />} />
-          <Route path='/smart-home' element={<SmartHomePage />} />
-          <Route path='/settings' element={<SettingsPage />} />
-          <Route path='*' element={<Navigate to='/' replace />} />
-        </Routes>
-      </div>
-
-      <BottomNav />
-    </div>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/live' element={<LivePage />} />
+      <Route path='/camera-host' element={<CameraHostPage />} />
+      <Route path='/emotion' element={<EmotionPage />} />
+      <Route path='/smart-home' element={<SmartHomePage />} />
+      <Route path='/settings' element={<SettingsPage />} />
+      <Route path='*' element={<Navigate to='/' replace />} />
+    </Routes>
   )
 }
 
