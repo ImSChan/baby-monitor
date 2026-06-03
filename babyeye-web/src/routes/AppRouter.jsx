dbb44c44312f1f9d3ed5c92d4e6953b02389697c
ImSChan <kxkx55402@gmail.com>
2026-06-03 19:04:31 +0900

    Fix camera host route

--- babyeye-web/src/routes/AppRouter.jsx
+++ babyeye-web/src/routes/AppRouter.jsx
@@ -1,22 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
+﻿import { Navigate, Route, Routes } from 'react-router-dom'
 
-import HomePage from '../pages/HomePage'
-import LivePage from '../pages/LivePage'
+import BottomNav from '../components/layout/BottomNav'
 import CameraHostPage from '../pages/CameraHostPage'
 import EmotionPage from '../pages/EmotionPage'
-import SmartHomePage from '../pages/SmartHomePage'
+import HomePage from '../pages/HomePage'
+import LivePage from '../pages/LivePage'
 import SettingsPage from '../pages/SettingsPage'
+import SmartHomePage from '../pages/SmartHomePage'
 
 function AppRouter() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/live' element={<LivePage />
-        <Route path='/camera-host' element={<CameraHostPage />} />} />
-      <Route path='/emotion' element={<EmotionPage />} />
-      <Route path='/smart-home' element={<SmartHomePage />} />
-      <Route path='/settings' element={<SettingsPage />} />
-    </Routes>
+    <div className='mx-auto min-h-screen max-w-[430px] bg-slate-950 text-white'>
+      <div className='pb-24'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/live' element={<LivePage />} />
+          <Route path='/camera-host' element={<CameraHostPage />} />
+          <Route path='/emotion' element={<EmotionPage />} />
+          <Route path='/smart-home' element={<SmartHomePage />} />
+          <Route path='/settings' element={<SettingsPage />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </div>
+
+      <BottomNav />
+    </div>
   )
 }
 
